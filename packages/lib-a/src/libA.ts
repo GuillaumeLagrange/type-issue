de5489c0bca1a55c84d99403bafb54b8687e8b5b
Guillaume Lagrange <guillaume.lagrange.ext@axaclimate.com>
2023-02-10 16:36:29 +0100

    chore: repro issue

--- packages/lib-a/src/libA.ts
+++ packages/lib-a/src/libA.ts
@@ -1,3 +1,19 @@
+import { libB, okLibB } from '@my-app/lib-b';
+
 export const libA = (): string => {
+  // Does not trigger an error when called in a function
+  const inFunctionLibB = libB();
+  void inFunctionLibB;
+
   return 'ok!';
 };
+
+// Does not trigger an error when not exported
+const notExportedLibB = libB();
+void notExportedLibB;
+
+// Triggers an error when exported
+export const exportedLibB = libB();
+
+// Does not trigger an error when exported
+export const exportedOkLibB = okLibB();
--- packages/lib-b/src/libB.ts
+++ packages/lib-b/src/libB.ts
@@ -1,3 +1,11 @@
-export const libB = (): string => {
-  return 'ok!';
+import random, { Random } from 'random';
+
+export { default as random } from 'random';
+
+export const libB = (): Random => {
+  return random;
+};
+
+export const okLibB = (): { ok: string } => {
+  return { ok: 'ok' };
 };
